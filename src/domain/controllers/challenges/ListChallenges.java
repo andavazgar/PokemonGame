35806f4a495a2d1fd1e62a5ed098c3d837a940d8
Andrés Vazquez <andavazgar.dev@gmail.com>
2018-11-29 20:49:32 -0500

    Moved page controllers

---
+++ src/domain/controllers/challenges/ListChallenges.java
@@ -0,0 +1,50 @@
+/*
+ * author: Andres Vazquez (#40007182)
+ * SOEN 387
+ */
+
+package domain.commands.challenges;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.JSONObject;
+import rdg.ChallengeRDG;
+
+/**
+ * Servlet implementation class ListChallenges
+ */
+@WebServlet("/ListChallenges")
+public class ListChallenges extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public ListChallenges() {
+        super();
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getSession(true).getAttribute("userID") == null) {
+			request.setAttribute("message", "You need to be logged in in order to view challenges.");
+			request.getRequestDispatcher("WEB-INF/jsp/fail.jsp").forward(request, response);
+			return;
+		}
+		
+		List<ChallengeTDG> challenges = ChallengeTDG.findAll();
+		JSONObject jsonObj = new JSONObject();
+		
+		jsonObj.put("challenges", challenges);
+		
+		request.setAttribute("jsonObj", jsonObj.getJSON());
+		request.getRequestDispatcher("WEB-INF/jsp/outputJSON.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
---
+++ src/domain/controllers/game/Retire.java
@@ -0,0 +1,81 @@
+/*
+ * author: Andres Vazquez (#40007182)
+ * SOEN 387
+ */
+
+package domain.commands.game;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Board;
+import models.enums.GameStatus;
+import models.enums.PlayerStatus;
+import rdg.GameRDG;
+
+/**
+ * Servlet implementation class Retire
+ */
+@WebServlet("/Retire")
+public class Retire extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+    public Retire() {
+        super();
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getSession(true).getAttribute("userID") == null) {
+			request.setAttribute("message", "You need to be logged in to view the games.");
+			request.getRequestDispatcher("WEB-INF/jsp/fail.jsp").forward(request, response);
+			return;
+		}
+		
+		String gameIDStr = request.getParameter("game");
+		
+		if(gameIDStr == null) {
+			request.setAttribute("message", "Please provide a game ID.");
+			request.getRequestDispatcher("WEB-INF/jsp/fail.jsp").forward(request, response);
+			return;
+		}
+		
+		int userID = (int) request.getSession(true).getAttribute("userID");
+		int gameID = Integer.parseInt(gameIDStr);
+		Board board = Board.findBoard(gameID);
+		int[] players = board.getPlayers();
+		
+		if(userID != players[0] && userID != players[1]) {
+			request.setAttribute("message", "You can only retire from a game you are part of.");
+			request.getRequestDispatcher("WEB-INF/jsp/fail.jsp").forward(request, response);
+			return;
+		}
+		
+		GameTDG game = GameTDG.find(gameID);
+		
+		if(board.getPlayers()[0] == userID) {
+			// The challenger retired
+			game.setChallengerStatus(PlayerStatus.retired.ordinal());
+		}
+		else {
+			// The challengee retired
+			game.setChallengeeStatus(PlayerStatus.retired.ordinal());
+		}
+		
+		game.setGameStatus(GameStatus.finished.ordinal());
+		game.update();
+		
+		request.setAttribute("message", "You retired from the game. This game is now finished.");
+		request.getRequestDispatcher("WEB-INF/jsp/success.jsp").forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
